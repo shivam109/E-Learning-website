19b0d9469f09b24586b67a31c6fd090e232bd2e8
shivam <76094061+shivam109@users.noreply.github.com>
2024-03-13 00:19:21 +0530

    Add files via upload

---
+++ LMS/user_login.py
@@ -0,0 +1,49 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from app.EmailBackEnd import EmailBackEnd
+from django.contrib.auth import authenticate, login, logout
+
+
+def REGISTER(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # check email
+        if User.objects.filter(email=email).exists():
+            messages.warning(request, 'Email are Already Exists !')
+            return redirect('register')
+
+        # check username
+        if User.objects.filter(username=username).exists():
+            messages.warning(request, 'Username are Already exists !')
+            return redirect('register')
+
+        user = User(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+        return redirect('login')
+
+        print(username, email, password)
+    return render(request, 'registration/register.html')
+
+
+def DO_LOGIN(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = EmailBackEnd.authenticate(request,
+                                         username=email,
+                                         password=password)
+        if user != None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Email and Password Are Invalid !')
+            return redirect('login')
---
+++ LMS/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import redirect, render
+
+
+def BASE(request):
+    return render(request, 'base.html')
+
+
+def HOME(request):
+    return render(request, 'Main/home.html')
+
+
+def SINGLE_COURSE(request):
+    return render(request, 'Main/single_course.html')
+
+
+def CONTACT_US(request):
+    return render(request, 'Main/contact_us.html')
+
+
+def ABOUT_US(request):
+    return render(request, 'Main/about_us.html')
+
+
+def DO_LOGIN(request):
+    return None
